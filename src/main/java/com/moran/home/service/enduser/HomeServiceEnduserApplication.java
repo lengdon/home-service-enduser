7fa4c37494fb9225f7c451e4586d9ec18935a535
Rahul <raahool_16@rediffmail.com>
2018-07-05 15:00:19 +0530

    Initial commit

---
+++ src/main/java/com/moran/home/service/enduser/HomeServiceEnduserApplication.java
@@ -0,0 +1,12 @@
+package com.moran.home.service.enduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceEnduserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceEnduserApplication.class, args);
+	}
+}
