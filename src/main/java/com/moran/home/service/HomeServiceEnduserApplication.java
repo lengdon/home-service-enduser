ae7487309cfc17d191b75daff036f06b565f8640
Rahul <raahool_16@rediffmail.com>
2018-07-09 23:21:31 +0530

    Continued!!

--- src/main/java/com/moran/home/service/HomeServiceEnduserApplication.java
+++ src/main/java/com/moran/home/service/HomeServiceEnduserApplication.java
@@ -1,4 +1,4 @@
-package com.moran.home.service.enduser;
+package com.moran.home.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
